d52db83886cc16226d80d54a75fc519f2d86aa94
Michał Kasperek <michal.kasperek2000@wp.pl>
2024-04-19 22:58:59 +0200

    Initial setup of basic UI

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello world!")
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Michal Kasperek - olx web-scrap")
+provinces = []
+provinces.insert(0,"Wszystkie")
+
+provinces_label = ttk.Label(root, text="Wybierz województwo:", font=("Times New Roman", 10))
+provinces_label.pack()
+provinces_combobox = ttk.Combobox(root, values = provinces, state='readonly')
+provinces_combobox.current(0)
+provinces_combobox.pack()
+
+table = ttk.Treeview(root, columns=('Data dodania', 'Tytuł', 'Miasto', 'Wynagrodzenie', 'Link'), show='headings')
+table.heading('Data dodania', text='Data dodania')
+table.heading('Tytuł', text='Tytuł')
+table.heading('Miasto', text='Miasto')
+table.heading('Wynagrodzenie', text='Wynagrodzenie')
+table.heading('Link', text='Link')
+table.pack(expand=True, fill='both')
+
+fetch_button = tk.Button(root, text="Pobierz oferty pracy", command="")
+fetch_button.pack()
+
+root.mainloop()
